35593ae365c5490caa051b86fec3ab3a4bda10f1
Cassandra <cassandra@beelen.one>
2019-10-12 00:49:06 +0200

    Add additional groups to my user

--- users.nix
+++ users.nix
@@ -6,7 +6,7 @@
     users.users.pandentia = {
         isNormalUser = true;
         description = "Cassandra";
-        extraGroups = [ "wheel" ];
+        extraGroups = [ "wheel" "docker" "syncthing" ];
         shell = pkgs.fish;
     };
     programs.fish.enable = true;
